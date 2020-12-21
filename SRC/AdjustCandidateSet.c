5bf572a507bd449b9342f97aaaa98f5e1c421112
Hust-Jzzheng <75603032+Hust-Jzzheng@users.noreply.github.com>
2020-12-21 20:57:47 +0800

    Update AdjustCandidateSet.c

--- SRC/AdjustCandidateSet.c
+++ SRC/AdjustCandidateSet.c
@@ -55,9 +55,7 @@ void AdjustCandidateSet()
                 /* Move the edge to the start of the candidate table */
                 NFrom->Value = LowerBound / ((double)(Distance(From, To) + NFrom->Alpha));
                 Temp = *NFrom;
-                for (NN = NFrom - 1; NN >= From->CandidateSet &&
-                    (Temp.Value > NN->Value ||
-                    (Temp.Value == NN->Value && Temp.Alpha < NN->Alpha)); NN--)
+                for (NN = NFrom - 1; NN >= From->CandidateSet; NN--)
                     *(NN + 1) = *NN;
                 *(NN + 1) = Temp;
             }
